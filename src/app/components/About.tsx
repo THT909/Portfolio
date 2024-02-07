8ebf9ca0a8f1fa960556a2cff817cd919052921c
Thanh <hiepthanha7@gmail.com>
2024-02-07 22:18:07 +0700

    style: style about section

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -3,19 +3,31 @@ import style from '../style/About.module.css';
 const About = () => {
     return (
         <div className={style.about}>
-            <div className={style.right_content}>
-                <div className={style.title}>Hi, I am Trầm Hiệp Thành</div>
-                <div className={style.sub}>I am a Android Developer|</div>
-                <div className={style.description}>
-                    I am a motivated and versatile individual, always eager to take on new
-                    challenges. With a passion for learning I am dedicated to delivering
-                    high-quality results. With a positive attitude and a growth mindset, I am ready
-                    to make a meaningful contribution and achieve great things.
+            <div className={style.pd}>
+                <div className={style.main_content}>
+                    <div className={style.right_content}>
+                        <div className={style.title}>
+                            Hi, I am
+                            <br />
+                            Trầm Hiệp Thành
+                        </div>
+                        <div className={style.sub}>
+                            I am a<span> Web developer</span>
+                        </div>
+                        <div className={style.description}>
+                            I am a motivated and versatile individual, always eager to take on new
+                            challenges. With a passion for learning I am dedicated to delivering
+                            high-quality results. With a positive attitude and a growth mindset, I
+                            am ready to make a meaningful contribution and achieve great things.
+                        </div>
+                        <div>
+                            <button className={style.btn_check_resume}>Check resume</button>
+                        </div>
+                    </div>
+                    <div className={style.left_content}>
+                        <div className={style.image}></div>
+                    </div>
                 </div>
-                <button className={style.btn_check_resume}>Check resume</button>
-            </div>
-            <div className={style.left_content}>
-                <div className={style.image}></div>
             </div>
         </div>
     );
--- src/app/components/Navigation.tsx
+++ src/app/components/Navigation.tsx
@@ -3,20 +3,22 @@ import Icon from '../utils/utils';
 const Navigation = () => {
     return (
         <div className={style.header}>
-            <div className={style.icon}>
-                <a href="#">
-                    <Icon />
-                </a>
-                <div className={style.portfolio}>Portfolio</div>
+            <div className={style.pd}>
+                <div className={style.icon}>
+                    <a href="#">
+                        <Icon />
+                    </a>
+                    <div className={style.portfolio}>Portfolio</div>
+                </div>
+                <div className={style.title}>
+                    <div className={style.text_header}>About</div>
+                    <div className={style.text_header}>Skills</div>
+                    <div className={style.text_header}>Experience</div>
+                    <div className={style.text_header}>Project</div>
+                    <div className={style.text_header}>Education</div>
+                </div>
+                <button className={style.bnt_git}>Github Profile</button>
             </div>
-            <div className={style.title}>
-                <div className={style.text_header}>About</div>
-                <div className={style.text_header}>Skills</div>
-                <div className={style.text_header}>Experience</div>
-                <div className={style.text_header}>Project</div>
-                <div className={style.text_header}>Education</div>
-            </div>
-            <button className={style.bnt_git}>Github Profile</button>
         </div>
     );
 };
