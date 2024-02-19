3f7c8db8b9c6c308246ca9f50052ce35865915f1
THT909 <hiepthanha7@gmail.com>
2024-02-19 23:28:33 +0700

    deploy 4

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,6 @@
-module.exports = {
-    exportPathMap: async function () {
-        return {
-            '/': { page: '/' }, // Trang chính
-
-            '/navigation': { page: '/navigation' }, // Section: Navigation
-            '/about': { page: '/about' }, // Section: About
-            '/experience': { page: '/experience' }, // Section: Experience
-            '/skill': { page: '/skill' }, // Section: Skill
-            '/education': { page: '/education' }, // Section: Education
-            '/footer': { page: '/footer' }, // Section: Footer
-            '/project': { page: '/project' }, // Section: Project
-        };
-    },
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export',
 };
+
+export default nextConfig;
