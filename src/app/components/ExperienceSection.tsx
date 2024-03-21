149ce3e718398df60678ab326b33744e444287b3
THT909 <hiepthanha7@gmail.com>
2024-03-21 09:00:18 +0700

    style:change experince

--- src/app/components/ExperienceSection.tsx
+++ src/app/components/ExperienceSection.tsx
@@ -48,6 +48,37 @@ const Experience = () => {
                                 </div>
                             </div>
                         </li>
+                        <li className={style.item}>
+                            <div className={style.line_space}>
+                                <span className={style.line_circle}></span>
+                                <span className={style.main_line}></span>
+                            </div>
+                            <div className={style.tag}>
+                                <div className={style.title_tag}>
+                                    <div className={style.image_tag}>
+                                        <Image alt="" src={reactjs}></Image>
+                                    </div>
+                                    <div className={style.title}>
+                                        <div className={style.main_title}>BackEnd Intern</div>
+                                        <div className={style.sub_title}>SGOD</div>
+                                        <div className={style.date_title}>03/2024 - now</div>
+                                    </div>
+                                </div>
+                                <div className={style.content}>
+                                    <span className={style.main_content}>Comming soon ,.....</span>
+                                    <br />
+                                    <div className={style.skill}>
+                                        <b>Skills:</b>
+                                        <div className={style.skill_ls}>
+                                            <div className={style.skill_name}>NestJS,</div>
+                                            <div className={style.skill_name}>MongoDB</div>
+                                            <div className={style.skill_name}>API</div>
+                                            <div className={style.skill_name}>Jwt</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
                     </ul>
                 </div>
             </div>
